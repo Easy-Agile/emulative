06ce46a146f19c43bb882a6c9912ea19d5660243
John Folder <john.folder@easyagile.com>
2021-10-13 08:47:44 +1100

    First commit: Functioning with four commands

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export enum Commands {
+    copyObject = 'emulative.copyObject',
+    copyObjectScratchFile = 'emulative.copyObjectScratchFile',
+    copyJson = 'emulative.copyJson',
+    copyJsonScratchFile = 'emulative.copyJsonScratchFile',
+    copyTestDataBuilder = 'emulative.copyTestDataBuilder',
+    testDataBuilderScratchFile = 'emulative.testDataBuilderScratchFile',
+}
---
+++ src/lib.ts
@@ -0,0 +1,16 @@
+import { readFile } from "fs/promises";
+
+export type FileTuple = [string, string];
+export type FileTuples = FileTuple[];
+
+export function readFiles(files: string[]): Promise<FileTuples> {
+  const filePromises = files.map(file => readFile(file));
+  return new Promise((resolve) => {
+    Promise.all(filePromises).then(buffers => {
+      const contents: string[][] = [];
+      buffers.forEach(
+          (buffer, index) => contents.push([files[index], buffer.toString()]));
+      resolve(contents as FileTuples);
+    });
+  });
+}
