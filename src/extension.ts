5e6f2a34f50d0c156c7a36d1b5f3a28c479c2c6b
John Folder <john.folder@easyagile.com>
2021-10-13 11:27:09 +1100

    Able to override properties with launch.json configuration env variables

--- src/extension.ts
+++ src/extension.ts
@@ -1,233 +1,184 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { writeSync } from 'clipboardy';
-import { mock as IntermockTS } from 'intermock';
-import { readFiles } from './lib';
-import { Commands } from './constants';
+import * as vscode from "vscode";
+import { workspace } from "vscode";
+import { Commands } from "./constants";
+import {
+  applyEmulativeOverrides,
+  buildMock,
+  convertToJSObjectAsString,
+  copyToClipboard,
+  createAsBuilder,
+  createAsVariable,
+  createScratchFile,
+  getEmulativePropertyOverrides,
+  getTargetObject,
+  initialiseAndGetData,
+} from "./lib";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "emulative" is now active!');
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "emulative" is now active!');
 
-	// The commandId parameter must match the command field in package.json
-	const disposableCopyObject = vscode.commands.registerCommand(Commands.copyObject, async function () {
-		// Get initial data
-		const initialData = initialiseAndGetData();
-		if (!initialData || !initialData.interfaceName || !initialData.path) {
-			return;
-		}
-		// Get the mock object
-		const mock = await buildMock(initialData.path, initialData.interfaceName);
-		// Convert to object as string
-		const targetObject = getTargetObject(mock, initialData.interfaceName);
-		if (!targetObject) {
-			vscode.window.showErrorMessage('Unable to create mock object.');
-			return;
-		}
-		//@ts-ignore
-		const mockObjectAsString = convertToJSObjectAsString(targetObject);
+  // launch.json configuration
+  const config = workspace.getConfiguration(
+    "launch",
+    vscode.window.activeTextEditor?.document.uri
+  );
 
-		// copy to clipboard
-		copyToClipboard(mockObjectAsString);
+  // retrieve values
+  const configurationValues: any = config.get("configurations");
 
-		// Notify user
-		vscode.window.showInformationMessage(`Mock object copied to the clipboard: ${mockObjectAsString}`);
-	});
+  const emulativeOverrides = getEmulativePropertyOverrides(configurationValues);
 
-	context.subscriptions.push(disposableCopyObject);
+  // The commandId parameter must match the command field in package.json
+  const disposableCopyObject = vscode.commands.registerCommand(
+    Commands.copyObject,
+    async function () {
+      // Get initial data
+      const initialData = initialiseAndGetData();
+      if (!initialData || !initialData.interfaceName || !initialData.path) {
+        return;
+      }
+      // Get the mock object
+      const mock = await buildMock(initialData.path, initialData.interfaceName);
+      const targetObject = getTargetObject(mock, initialData.interfaceName);
+      if (!targetObject) {
+        vscode.window.showErrorMessage("Unable to create mock object.");
+        return;
+      }
 
-	const disposableCopyObjectScratchFile = vscode.commands.registerCommand(Commands.copyObjectScratchFile, async function () {
-		// Get initial data
-		const initialData = initialiseAndGetData();
-		if (!initialData || !initialData.interfaceName || !initialData.path) {
-			return;
-		}
-		// Get the mock object
-		const mock = await buildMock(initialData.path, initialData.interfaceName);
-		// Convert to object as string
-		const targetObject = getTargetObject(mock, initialData.interfaceName);
-		if (!targetObject) {
-			vscode.window.showErrorMessage('Unable to create mock object.');
-			return;
-		}
-		//@ts-ignore
-		const mockObjectAsString = convertToJSObjectAsString(targetObject);
+      applyEmulativeOverrides(emulativeOverrides, targetObject);
 
-		const objectAsVariableString = createAsVariable(initialData.interfaceName, mockObjectAsString);
-		// copy to clipboard
-		copyToClipboard(objectAsVariableString);
+      const mockObjectAsString = convertToJSObjectAsString(targetObject);
 
-		createScratchFile(objectAsVariableString);
+      // copy to clipboard
+      copyToClipboard(mockObjectAsString);
 
-		// Notify user
-		vscode.window.showInformationMessage(`Scratch file with mock object created: ${mockObjectAsString}`);
-	});
+      // Notify user
+      vscode.window.showInformationMessage(
+        `Mock object copied to the clipboard: ${mockObjectAsString}`
+      );
+    }
+  );
 
-	context.subscriptions.push(disposableCopyObjectScratchFile);
+  context.subscriptions.push(disposableCopyObject);
 
-	const disposableCopyJson = vscode.commands.registerCommand(Commands.copyJson, async function () {
-		// Get initial data
-		const initialData = initialiseAndGetData();
-		if (!initialData || !initialData.interfaceName || !initialData.path) {
-			return;
-		}
-		// Get the mock object
-		const mock = await buildMock(initialData.path, initialData.interfaceName);
-		// Convert to object as string
-		const targetObject = getTargetObject(mock, initialData.interfaceName);
-		if (!targetObject) {
-			vscode.window.showErrorMessage('Unable to create mock object.');
-			return;
-		}
-		//@ts-ignore
-		const mockObjectStringified = JSON.stringify(targetObject);
+  const disposableCopyObjectScratchFile = vscode.commands.registerCommand(
+    Commands.copyObjectScratchFile,
+    async function () {
+      // Get initial data
+      const initialData = initialiseAndGetData();
+      if (!initialData || !initialData.interfaceName || !initialData.path) {
+        return;
+      }
+      // Get the mock object
+      const mock = await buildMock(initialData.path, initialData.interfaceName);
+      // Convert to object as string
+      const targetObject = getTargetObject(mock, initialData.interfaceName);
+      if (!targetObject) {
+        vscode.window.showErrorMessage("Unable to create mock object.");
+        return;
+      }
 
-		// copy to clipboard
-		copyToClipboard(mockObjectStringified);
+      applyEmulativeOverrides(emulativeOverrides, targetObject);
 
-		// Notify user
-		vscode.window.showInformationMessage(`Mock JSON object copied to the clipboard: ${mockObjectStringified}`);
-	});
+      //@ts-ignore
+      const mockObjectAsString = convertToJSObjectAsString(targetObject);
 
-	context.subscriptions.push(disposableCopyJson);
+      const objectAsVariableString = createAsVariable(
+        initialData.interfaceName,
+        mockObjectAsString
+      );
+      // copy to clipboard
+      copyToClipboard(objectAsVariableString);
 
-	const disposableCopyTestBuilder = vscode.commands.registerCommand(Commands.copyTestDataBuilder, async function () {
-		// Get initial data
-		const initialData = initialiseAndGetData();
-		if (!initialData || !initialData.interfaceName || !initialData.path) {
-			return;
-		}
-		// Get the mock object
-		const mock = await buildMock(initialData.path, initialData.interfaceName);
-		// Convert to object as string
-		const targetObject = getTargetObject(mock, initialData.interfaceName);
-		if (!targetObject) {
-			vscode.window.showErrorMessage('Unable to create mock object.');
-			return;
-		}
-		//@ts-ignore
-		const mockObjectAsString = convertToJSObjectAsString(targetObject);
+      createScratchFile(objectAsVariableString);
 
-		const builderFunctionAsString = createAsBuilder(initialData.interfaceName, mockObjectAsString);
+      // Notify user
+      vscode.window.showInformationMessage(
+        `Scratch file with mock object created: ${mockObjectAsString}`
+      );
+    }
+  );
 
-		// copy to clipboard
-		copyToClipboard(builderFunctionAsString);
+  context.subscriptions.push(disposableCopyObjectScratchFile);
 
-		// Notify user
-		vscode.window.showInformationMessage(`builder function for a mock object of type ${initialData.interfaceName} copied to the clipboard: ${builderFunctionAsString}`);
-	});
+  const disposableCopyJson = vscode.commands.registerCommand(
+    Commands.copyJson,
+    async function () {
+      // Get initial data
+      const initialData = initialiseAndGetData();
+      if (!initialData || !initialData.interfaceName || !initialData.path) {
+        return;
+      }
+      // Get the mock object
+      const mock = await buildMock(initialData.path, initialData.interfaceName);
+      // Convert to object as string
+      const targetObject = getTargetObject(mock, initialData.interfaceName);
+      if (!targetObject) {
+        vscode.window.showErrorMessage("Unable to create mock object.");
+        return;
+      }
 
-	context.subscriptions.push(disposableCopyTestBuilder);
+      applyEmulativeOverrides(emulativeOverrides, targetObject);
+
+      //@ts-ignore
+      const mockObjectStringified = JSON.stringify(targetObject);
+
+      // copy to clipboard
+      copyToClipboard(mockObjectStringified);
+
+      // Notify user
+      vscode.window.showInformationMessage(
+        `Mock JSON object copied to the clipboard: ${mockObjectStringified}`
+      );
+    }
+  );
+
+  context.subscriptions.push(disposableCopyJson);
+
+  const disposableCopyTestBuilder = vscode.commands.registerCommand(
+    Commands.copyTestDataBuilder,
+    async function () {
+      // Get initial data
+      const initialData = initialiseAndGetData();
+      if (!initialData || !initialData.interfaceName || !initialData.path) {
+        return;
+      }
+      // Get the mock object
+      const mock = await buildMock(initialData.path, initialData.interfaceName);
+      // Convert to object as string
+      const targetObject = getTargetObject(mock, initialData.interfaceName);
+      if (!targetObject) {
+        vscode.window.showErrorMessage("Unable to create mock object.");
+        return;
+      }
+
+      applyEmulativeOverrides(emulativeOverrides, targetObject);
+
+      //@ts-ignore
+      const mockObjectAsString = convertToJSObjectAsString(targetObject);
+
+      const builderFunctionAsString = createAsBuilder(
+        initialData.interfaceName,
+        mockObjectAsString
+      );
+
+      // copy to clipboard
+      copyToClipboard(builderFunctionAsString);
+
+      // Notify user
+      vscode.window.showInformationMessage(
+        `builder function for a mock object of type ${initialData.interfaceName} copied to the clipboard: ${builderFunctionAsString}`
+      );
+    }
+  );
+
+  context.subscriptions.push(disposableCopyTestBuilder);
 }
-
-const initialiseAndGetData = () => {
-	// Get the active text editor
-	const editor = vscode.window.activeTextEditor;
-
-	if (editor) {
-		const document = editor.document;
-		const selection = editor.selection;
-
-		// Get the word within the selection
-		const interfaceName = document.getText(selection);
-		const path  = vscode.window.activeTextEditor?.document.fileName;
-
-		if (!path) {
-			vscode.window.showErrorMessage('No path found.');
-			return;
-		}
-
-		if (!interfaceName) {
-			vscode.window.showErrorMessage('No interface name found.');
-			return;
-		}
-
-		return {
-			interfaceName,
-			path
-		};
-		} else {
-			vscode.window.showErrorMessage('No editor found.');
-		}
-};
-
-const getTargetObject = (mockObject: any, interfaceName: string) => {
-	return mockObject[interfaceName];
-};
-
-const buildMock = (path: string, interfaceName: string) => {
-	return getMockObject(path, interfaceName);
-};
-
-const sendResultToTerminal = (mockObject: any) => {
-	let terminalToUse = vscode.window.activeTerminal;
-	// eslint-disable-next-line eqeqeq
-	if (terminalToUse == null) {
-		let NEXT_TERM_ID = 1;
-		terminalToUse = vscode.window.createTerminal(`Ext Terminal #${NEXT_TERM_ID++}`);
-	}
-
-	terminalToUse.show();
-	terminalToUse.sendText(`echo ${mockObject}`);
-};
-
-const getMockObject = async (path: string, interfaceName: string) => {
-	try {
-		const files = await readFiles([path]);
-
-		const result = IntermockTS({
-			files,
-			interfaces: [interfaceName],
-			isFixedMode: false,
-			output: 'object',
-		}) as string;
-
-		return result;
-	  } catch (err) {
-		  //@ts-ignore
-		console.log(err.message);
-	  }
-};
-
-const createAsBuilder = (interfaceName: string, mockObjectAsString: string) => {
-	return `export const build${interfaceName} = (overrides?: Partial<${interfaceName}>): ${interfaceName} => {
-		const default${interfaceName} = ${mockObjectAsString};
-
-		return { ...default${interfaceName}, ...overrides };
-	};`;
-};
-
-const createAsVariable = (interfaceName: string, mockObjectAsString: string) => {
-	const objectIdentifierWithPreferredCase = 'const ' + interfaceName[0].toLowerCase() + interfaceName.slice(1) + 'Mock = ';
-	return objectIdentifierWithPreferredCase + mockObjectAsString + ';';
-};
-
-const copyToClipboard = (mock: string) => {
-	writeSync(mock);
-
-};
-
-const createScratchFile = (mock: string) => {
-	const documentOptions = {
-		content: mock, 
-		language: 'typescript'
-	};
-
-	vscode.workspace.openTextDocument(documentOptions).then(
-		document => vscode.window.showTextDocument(document));
-};
-
-const convertToJSObjectAsString = (obj: any) => {
-    var cleaned = JSON.stringify(obj, null, 2);
-
-    return cleaned.replace(/^[\t ]*"[^:\n\r]+(?<!\\)":/gm, function (match) {
-        return match.replace(/"/g, "");
-    });
-};
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
--- src/lib.ts
+++ src/lib.ts
@@ -1,16 +1,178 @@
 import { readFile } from "fs/promises";
+import * as vscode from "vscode";
+import { writeSync } from "clipboardy";
+import { mock as IntermockTS } from "intermock";
+import { filter, map } from "lodash";
 
 export type FileTuple = [string, string];
 export type FileTuples = FileTuple[];
 
 export function readFiles(files: string[]): Promise<FileTuples> {
-  const filePromises = files.map(file => readFile(file));
+  const filePromises = files.map((file) => readFile(file));
   return new Promise((resolve) => {
-    Promise.all(filePromises).then(buffers => {
+    Promise.all(filePromises).then((buffers) => {
       const contents: string[][] = [];
-      buffers.forEach(
-          (buffer, index) => contents.push([files[index], buffer.toString()]));
+      buffers.forEach((buffer, index) =>
+        contents.push([files[index], buffer.toString()])
+      );
       resolve(contents as FileTuples);
     });
   });
 }
+
+export const initialiseAndGetData = () => {
+  // Get the active text editor
+  const editor = vscode.window.activeTextEditor;
+
+  if (editor) {
+    const document = editor.document;
+    const selection = editor.selection;
+
+    // Get the word within the selection
+    const interfaceName = document.getText(selection);
+    const path = vscode.window.activeTextEditor?.document.fileName;
+
+    if (!path) {
+      vscode.window.showErrorMessage("No path found.");
+      return;
+    }
+
+    if (!interfaceName) {
+      vscode.window.showErrorMessage("No interface name found.");
+      return;
+    }
+
+    return {
+      interfaceName,
+      path,
+    };
+  } else {
+    vscode.window.showErrorMessage("No editor found.");
+  }
+};
+
+export const getTargetObject = (mockObject: any, interfaceName: string) => {
+  return mockObject[interfaceName];
+};
+
+export const buildMock = (path: string, interfaceName: string) => {
+  return getMockObject(path, interfaceName);
+};
+
+export const getMockObject = async (path: string, interfaceName: string) => {
+  try {
+    const files = await readFiles([path]);
+
+    const result = IntermockTS({
+      files,
+      interfaces: [interfaceName],
+      isFixedMode: false,
+      output: "object",
+    }) as string;
+
+    return result;
+  } catch (err) {
+    //@ts-ignore
+    console.log(err.message);
+  }
+};
+
+export const createAsBuilder = (
+  interfaceName: string,
+  mockObjectAsString: string
+) => {
+  return `export const build${interfaceName} = (overrides?: Partial<${interfaceName}>): ${interfaceName} => {
+		const default${interfaceName} = ${mockObjectAsString};
+
+		return { ...default${interfaceName}, ...overrides };
+	};`;
+};
+
+export const createAsVariable = (
+  interfaceName: string,
+  mockObjectAsString: string
+) => {
+  const objectIdentifierWithPreferredCase =
+    "const " +
+    interfaceName[0].toLowerCase() +
+    interfaceName.slice(1) +
+    "Mock = ";
+  return objectIdentifierWithPreferredCase + mockObjectAsString + ";";
+};
+
+export const copyToClipboard = (mock: string) => {
+  writeSync(mock);
+};
+
+export const createScratchFile = (mock: string) => {
+  const documentOptions = {
+    content: mock,
+    language: "typescript",
+  };
+
+  vscode.workspace
+    .openTextDocument(documentOptions)
+    .then((document) => vscode.window.showTextDocument(document));
+};
+
+export const convertToJSObjectAsString = (obj: any) => {
+  let cleaned = JSON.stringify(obj, null, 2);
+
+  return cleaned.replace(/^[\t ]*"[^:\n\r]+(?<!\\)":/gm, function (match) {
+    return match.replace(/"/g, "");
+  });
+};
+
+export const getEmulativePropertyOverrides = (
+  configurationVariablesArray: any[]
+) => {
+  const configurationVariables = configurationVariablesArray[0];
+  const emulativePropertyOverridesString: string =
+    configurationVariables?.env?.emulativePropertyOverrides;
+  const individualPropertiesandValues =
+    emulativePropertyOverridesString.split(",");
+  return individualPropertiesandValues.map((propValue) => {
+    const substrings = propValue.split(":");
+    return {
+      [substrings[0]]: substrings[1],
+    };
+  });
+};
+
+export const sendResultToTerminal = (mockObject: any) => {
+  let terminalToUse = vscode.window.activeTerminal;
+  // eslint-disable-next-line eqeqeq
+  if (terminalToUse == null) {
+    let NEXT_TERM_ID = 1;
+    terminalToUse = vscode.window.createTerminal(
+      `Ext Terminal #${NEXT_TERM_ID++}`
+    );
+  }
+
+  terminalToUse.show();
+  terminalToUse.sendText(`echo ${mockObject}`);
+};
+
+export const mutateObjectProperty = (prop: string, value: string, obj: any) => {
+  obj.constructor === Object &&
+    Object.keys(obj).forEach((key) => {
+      if (key === prop) {
+        obj[key] = value;
+      }
+      mutateObjectProperty(prop, value, obj[key]);
+    });
+};
+
+export const applyEmulativeOverrides = (
+  emulativeOverrides: any[],
+  targetObject: any
+) => {
+  if (emulativeOverrides) {
+    emulativeOverrides.forEach((override) => {
+      const prop = Object.keys(override)[0];
+      const value = override[prop];
+
+      mutateObjectProperty(prop, value, targetObject);
+    });
+  }
+};
